2f0cdbfbeb06c6d27eb9ed22e8454935f04c3683
Akshara <aksharar02@gmail.com>
2024-05-28 08:15:56 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import useStorage from "./useStorage";
+import "./App.css";
+
+function App() {
+  const [text, setText] = useStorage("key", "");
+  const handleText = (event) => {
+    console.log(event.target.value);
+    setText(event.target.value);
+  };
+  return (
+    <div className="page">
+      <input className="input" value={text} onChange={handleText} type="text" />
+    </div>
+  );
+}
+
+export default App;
